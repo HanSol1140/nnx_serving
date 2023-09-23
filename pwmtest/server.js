04e7e75bd32c4ec33bb75bce183edbbfd85197af
SongHanSol <skycriperd@gmail.com>
2023-09-23 06:38:50 +0100

    test

--- pwmtest/server.js
+++ pwmtest/server.js
@@ -12,3 +12,15 @@ const PORT = process.env.PORT || 8084;
 const server = app.listen(PORT, () => {
     console.log("서버시작 포트 : " + PORT);
 });
+//  sudo apt-get install pigpio
+const pigpio = require('pigpio');
+const Gpio = pigpio.Gpio;
+const pwmPin = new Gpio(18, { mode: Gpio.OUTPUT });
+let dutyCycle = 0;
+setInterval(() => {
+    pwmPin.pwmWrite(dutyCycle);
+    dutyCycle += 5;
+    if (dutyCycle > 255) {
+        dutyCycle = 0;
+    }
+}, 20);
