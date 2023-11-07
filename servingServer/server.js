1b25d1cb926d27c26e56569df4e43fcd2142c399
SongHanSol <skycriperd@gmail.com>
2023-11-07 18:52:14 +0900

    123

--- servingServer/server.js
+++ servingServer/server.js
@@ -68,54 +68,9 @@ setTimeout(() => {
     // }
     // console.log(mappingData);
     // console.log(laserCoordinate);
+    console.log(robotconfig_1.pointCoordinate);
 }, 1000);
 // ====================================================================================
-// setInterval(async () => {
-// try{
-//     for (var i in robotSettings) { // i = 등록된 로봇Name
-//         // 로봇 좌표 받기
-//         await Func.getPose(i);
-//         // console.log(i);
-//         // console.log(robotCoordinate[i]);
-//         // 로봇이 쏘는 레이저좌표 받기
-//         await Func.getLaser(i);
-//         // 교차로 체크
-//         const crossCheck = await Func.checkCrossRoad(i); // true / false반환
-//         if(crossCheck){ // 교차로
-//         }
-//         // 레이저 좌표를 받아서 충돌위험 체크
-//         const collision = await Func.detectCollision(i); // true / false반환
-//         if(collision){ // mapingServer에서 기록한 맵핑데이터에 의해 벽충돌은 제거함
-//             // // 장애물이 감지됫다면
-//             console.log(i + " 장애물 충돌 위험");
-//             if(currentRobotName == i){
-//                 console.log("본체앞에 장애물이 있습니다.");
-//             }
-//             // // console.log(collision);
-//             // console.log(collision); // 장애물 좌표
-//             // console.log(robotCoordinate["robot1"].x, robotCoordinate["robot1"].y); // 로봇 좌표
-//             // // 로봇인지 아닌지 체크
-//             const checkRobot = await Func.checkRobotCoordinates(i, collision);
-//             if (checkRobot){
-//                 console.log("로봇입니다");
-//             }else{
-//                 console.log("로봇이 아닙니다.");
-//             }
-//         }else{
-//             // 장애물 충돌 위험 없음
-//         }
-//         // console.log("======================================");
-//         // detectCollision 리턴값이 true(충돌위험발생)이라면 
-//         // console.log(robotCoordinate);
-//         // if(checkValue){
-//             //     // 체크한다
-//             //     // 
-//             // }
-//         }
-//     }catch(error){
-//         console.error("error");
-//     } 
-// }, 33);
 // ====================================================================================
 // ====================================================================================
 setInterval(() => __awaiter(void 0, void 0, void 0, function* () {
@@ -167,4 +122,52 @@ setInterval(() => __awaiter(void 0, void 0, void 0, function* () {
     }
 }), 33);
 // ====================================================================================
+// ====================================================================================
+// ====================================================================================
+// setInterval(async () => {
+// try{
+//     for (var i in robotSettings) { // i = 등록된 로봇Name
+//         // 로봇 좌표 받기
+//         await Func.getPose(i);
+//         // console.log(i);
+//         // console.log(robotCoordinate[i]);
+//         // 로봇이 쏘는 레이저좌표 받기
+//         await Func.getLaser(i);
+//         // 교차로 체크
+//         const crossCheck = await Func.checkCrossRoad(i); // true / false반환
+//         if(crossCheck){ // 교차로
+//         }
+//         // 레이저 좌표를 받아서 충돌위험 체크
+//         const collision = await Func.detectCollision(i); // true / false반환
+//         if(collision){ // mapingServer에서 기록한 맵핑데이터에 의해 벽충돌은 제거함
+//             // // 장애물이 감지됫다면
+//             console.log(i + " 장애물 충돌 위험");
+//             if(currentRobotName == i){
+//                 console.log("본체앞에 장애물이 있습니다.");
+//             }
+//             // // console.log(collision);
+//             // console.log(collision); // 장애물 좌표
+//             // console.log(robotCoordinate["robot1"].x, robotCoordinate["robot1"].y); // 로봇 좌표
+//             // // 로봇인지 아닌지 체크
+//             const checkRobot = await Func.checkRobotCoordinates(i, collision);
+//             if (checkRobot){
+//                 console.log("로봇입니다");
+//             }else{
+//                 console.log("로봇이 아닙니다.");
+//             }
+//         }else{
+//             // 장애물 충돌 위험 없음
+//         }
+//         // console.log("======================================");
+//         // detectCollision 리턴값이 true(충돌위험발생)이라면 
+//         // console.log(robotCoordinate);
+//         // if(checkValue){
+//             //     // 체크한다
+//             //     // 
+//             // }
+//         }
+//     }catch(error){
+//         console.error("error");
+//     } 
+// }, 33);
 // ====================================================================================
