3b565b08025ffbb52b41961214a9f888625ed162
SongHanSol <skycriperd@gmail.com>
2023-11-07 10:47:44 +0900

    123

--- servingServer/Services/robotWheelControll.ts
+++ servingServer/Services/robotWheelControll.ts
@@ -53,12 +53,11 @@ export async function wheelControll2() {
     uart2.on('readable', () => { 
     const data = uart2.read();
     if (data) {
-        adjustSpeedAndSend(data);
-        // let hexData1 = data.toString('hex').toUpperCase();
+        let hexData1 = data.toString('hex').toUpperCase();
         // // hexData1 = hexData1.match(/.{1,2}/g)
         // // let byteArray = hexData.match(/.{1,2}/g).map(byte => parseInt(byte, 16));
         // console.log(`Received from UART2: ${hexData1}`);
-        // uart3.write(data); 
+        uart3.write(data); 
     }
     }); 
     
@@ -68,7 +67,7 @@ export async function wheelControll2() {
         let hexData2 = data.toString('hex').toUpperCase();
         // hexData2 = hexData2.match(/.{1,2}/g)
         // let byteArray = hexData.match(/.{1,2}/g).map(byte => parseInt(byte, 16));
-        console.log(`Received from UART3: ${hexData2}`);
+        // console.log(`Received from UART3: ${hexData2}`);
         uart2.write(data); 
     }
     });
