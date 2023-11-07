1204c384fa57852b284be14077a1db7fc3805316
SongHanSol <skycriperd@gmail.com>
2023-11-07 09:11:28 +0900

    123123

--- servingServer/server.ts
+++ servingServer/server.ts
@@ -52,46 +52,46 @@ setTimeout(()=>{
 }, 1000);
 
 // ====================================================================================
-// let collision;
-// setInterval(async () => {
-// try{
-//     for (var i in robotSettings) { // i = 등록된 로봇Name
-//         // 로봇 좌표 받기
-//         await Func.getPose(i);
-//         // console.log(i);
-//         // console.log(robotCoordinate[i]);
-//         // 로봇이 쏘는 레이저좌표 받기
-//         await Func.getLaser(i);
-//         // 교차로 체크
-//         const crossCheck = await Func.checkCrossRoad(i); // true / false반환
-//         if(crossCheck){ // 교차로
-//         }
+let collision;
+setInterval(async () => {
+try{
+    for (var i in robotSettings) { // i = 등록된 로봇Name
+        // 로봇 좌표 받기
+        await Func.getPose(i);
+        // console.log(i);
+        // console.log(robotCoordinate[i]);
+        // 로봇이 쏘는 레이저좌표 받기
+        await Func.getLaser(i);
+        // 교차로 체크
+        const crossCheck = await Func.checkCrossRoad(i); // true / false반환
+        if(crossCheck){ // 교차로
+        }
         
-//         // 레이저 좌표를 받아서 충돌위험 체크
-//         collision = await Func.detectCollision(i); // true / false반환
-//         if(collision){ // mapingServer에서 기록한 맵핑데이터에 의해 벽충돌은 제거함
-//             //     // 장애물이 감지됫다면
-//             console.log(i + " 장애물 충돌 위험");
-//             //     // 로봇인지 아닌지 체크
-//             // console.log(collision);
-//             console.log(collision);
-//             console.log(robotCoordinate["robot1"].x, robotCoordinate["robot1"].y);
-//             Func.checkRobotCoordinates(i, collision);
-//         }
+        // 레이저 좌표를 받아서 충돌위험 체크
+        collision = await Func.detectCollision(i); // true / false반환
+        if(collision){ // mapingServer에서 기록한 맵핑데이터에 의해 벽충돌은 제거함
+            //     // 장애물이 감지됫다면
+            console.log(i + " 장애물 충돌 위험");
+            //     // 로봇인지 아닌지 체크
+            // console.log(collision);
+            console.log(collision);
+            console.log(robotCoordinate["robot1"].x, robotCoordinate["robot1"].y);
+            Func.checkRobotCoordinates(i, collision);
+        }
         
-//         console.log("======================================");
-//         // detectCollision 리턴값이 true(충돌위험발생)이라면 
-//         // console.log(robotCoordinate);
-//         // if(checkValue){
-//             //     // 체크한다
-//             //     // 
-//             // }
+        console.log("======================================");
+        // detectCollision 리턴값이 true(충돌위험발생)이라면 
+        // console.log(robotCoordinate);
+        // if(checkValue){
+            //     // 체크한다
+            //     // 
+            // }
             
-//         }
-//     }catch(error){
-//         console.error("error");
-//     } 
-// }, 33);
+        }
+    }catch(error){
+        console.error("error");
+    } 
+}, 33);
 // ====================================================================================
 const {SerialPort, ReadlineParser} = require('serialport');
 
@@ -114,38 +114,26 @@ uart2.pipe(parser3);
 //     uart2.write(data); 
 // });
 uart2.on('readable', () => {
-    const data = uart2.read();
-    if (data) {
-      const hexData = data.toString('hex').toUpperCase();
-      console.log(`Received from UART2: ${hexData}`);
-  
-      // Buffer로 바이트 배열 생성
-      const bytes = Buffer.from(data);
-      
-      // 각 바이트를 순차적으로 전송
-      bytes.forEach(byte => {
-        uart3.write(Buffer.from([byte]));
-      });
-    }
-  });
-  
+  const data = uart2.read();
+  if (data) {
+    let hexData = data.toString('hex').toUpperCase();
+    hexData = hexData.match(/.{1,2}/g).join(' ');
+    let byteArray = hexData.match(/.{1,2}/g).map(byte => parseInt(byte, 16));
+    console.log(`Received from UART2: ${hexData}`);
+    uart3.write(data); 
+  }
+}); 
  
-  uart3.on('readable', () => {
-    const data = uart3.read();
-    if (data) {
-      const hexData = data.toString('hex').toUpperCase();
-      console.log(`Received from UART3: ${hexData}`);
-  
-      // Buffer로 바이트 배열 생성
-      const bytes = Buffer.from(data);
-      
-      // 각 바이트를 순차적으로 전송
-      bytes.forEach(byte => {
-        uart2.write(Buffer.from([byte]));
-      });
-    }
-  });
-  
+uart3.on('readable', () => {
+  const data = uart3.read();
+  if (data) {
+    let hexData = data.toString('hex').toUpperCase();
+    hexData = hexData.match(/.{1,2}/g).join(' ');
+    let byteArray = hexData.match(/.{1,2}/g).map(byte => parseInt(byte, 16));
+    console.log(`Received from UART2: ${hexData}`);
+    uart2.write(data); 
+  }
+});
 
 // 에러 핸들링
 uart2.on('error', function(err:any) {
