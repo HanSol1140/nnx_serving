6859d3eb3e411cce733603d1da77185f23fb46fd
SongHanSol <skycriperd@gmail.com>
2023-11-09 14:28:10 +0900

    123123

--- servingServer/server.js
+++ servingServer/server.js
@@ -94,7 +94,7 @@ setInterval(() => __awaiter(void 0, void 0, void 0, function* () {
         // console.log(collisionCheck);
         if (collisionCheck) { // mapingServer에서 기록한 맵핑데이터에 의해 벽충돌은 제거함
             // setCollision(true);
-            server2.send({ booleanValue: false });
+            server2.send({ booleanValue: true });
             // // 장애물이 감지됫다면
             // console.log(currentRobotName + " 장애물 충돌 위험");
             // // // 로봇인지 아닌지 체크
