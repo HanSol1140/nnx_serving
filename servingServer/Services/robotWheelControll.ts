89af41cc73683e4b0f6d8cdba96de0a48d32a40d
SongHanSol <skycriperd@gmail.com>
2023-11-09 16:52:26 +0900

    123123

--- servingServer/Services/robotWheelControll.ts
+++ servingServer/Services/robotWheelControll.ts
@@ -84,7 +84,7 @@ function adjustSpeedAndSend(data: any) {
 
         // 속도 조정 (예시: 왼쪽 바퀴 50%, 오른쪽 바퀴 25%로 조정)
         let adjustedLeftWheelSpeed = Math.floor(leftWheelSpeed * 0.5);
-        let adjustedRightWheelSpeed = Math.floor(leftWheelSpeed * 0.25);
+        let adjustedRightWheelSpeed = Math.floor(leftWheelSpeed * 1);
 
         // 조정된 속도값으로 Buffer 업데이트
         commandBuffer[9] = (adjustedLeftWheelSpeed >> 8) + 0x80;
