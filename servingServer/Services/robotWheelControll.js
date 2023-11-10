9d0184e19af88851ad9c3831a09cfbb12edfd8cc
SongHanSol <skycriperd@gmail.com>
2023-11-10 18:14:19 +0900

    123

--- servingServer/Services/robotWheelControll.js
+++ servingServer/Services/robotWheelControll.js
@@ -66,7 +66,7 @@ function wheelControll() {
                 else {
                     // collision이 false일 때 정상 운행
                     // console.log("정상운행");
-                    const hexString = data.toString('hex'); // 16진수 데이터를 문자열로 변환
+                    const hexString = data.toString('hex').toUpperCase(); // 16진수 데이터를 문자열로 변환
                     console.log("uart2 : " + hexString);
                     checkForCollision();
                     uart3.write(data);
@@ -77,7 +77,7 @@ function wheelControll() {
         uart3.on('readable', () => {
             const data = uart3.read();
             if (data) {
-                const hexString = data.toString('hex'); // 16진수 데이터를 문자열로 변환
+                const hexString = data.toString('hex').toUpperCase(); // 16진수 데이터를 문자열로 변환
                 console.log("uart3 : " + hexString);
                 uart2.write(data);
             }
